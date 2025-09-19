834c170c2ac2dc0b5e02b75d83b23e03f29944df
bcontrerasgodinez-cmd <bcontrerasgodinez@mail.weber.edu>
2025-09-19 10:09:43 -0600

    Agregar archivo saludo.py

---
+++ saludo.py
@@ -0,0 +1,14 @@
+# conociendo a alguien 
+Nombre = input ("Hola ¿Como te llamas? ")
+print("Mucho gusto "+ Nombre)
+# pregunta por su estado de animo
+estadodeanimo = input("¿Como estas? ¿Bien o mal?")
+if estadodeanimo == "Bien":
+    print("😊")
+elif estadodeanimo == "mal":
+    print("😢")
+else:
+    print("😤")
+# preguntar por su pais o su lugar de origen
+Pais = input ("De donde eres?")
+print(Pais +"es muy bonito👌")
